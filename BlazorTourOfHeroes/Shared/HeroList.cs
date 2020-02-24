1a769cad1a6075014bef4525971639a687b1ebfa
Tobbe3108 <Tobbe3108@gmail.com>
2020-02-24 18:29:34 +0100

    Navbar

--- BlazorTourOfHeroes/Shared/HeroList.cs
+++ BlazorTourOfHeroes/Shared/HeroList.cs
@@ -17,8 +17,53 @@ namespace BlazorTourOfHeroes.Shared
         {
             new Hero()
             {
-                Id = 1,
-                Name = "Windstorm"
+                Id = 11,
+                Name =  "Dr Nice"
+            },
+            new Hero()
+            {
+                Id = 12,
+                Name = "Narco"
+            },
+            new Hero()
+            {
+                Id = 13,
+                Name = "Bombasto"
+            },
+            new Hero()
+            {
+                Id = 14,
+                Name = "Celeritas"
+            },
+            new Hero()
+            {
+                Id = 15,
+                Name = "Magneta"
+            },
+            new Hero()
+            {
+                Id = 16,
+                Name = "RubberMan"
+            },
+            new Hero()
+            {
+                Id = 17,
+                Name = "Dynama"
+            },
+            new Hero()
+            {
+                Id = 18,
+                Name = "Dr IQ"
+            },
+            new Hero()
+            {
+                Id = 19,
+                Name = "Magma"
+            },
+            new Hero()
+            {
+                Id = 20,
+                Name = "Tornado"
             }
         };
     }
